0b9e11a8ec28dadf9c4b74bf8e79f8d3b2a1d9fb
Rifqi Fauzi <rifqi.thefi.fauzi@gmail.com>
2021-07-01 17:41:12 +0700

    put mcp23s17 inside modules class

--- lib/wiringpi/modules/mcp23s17.rb
+++ lib/wiringpi/modules/mcp23s17.rb
@@ -1,14 +1,16 @@
 module WiringPi
-  class Mcp23s17 < ModuleBase
-    attr_reader :spi_port, :device_id
+  class Modules
+    class Mcp23s17 < ModuleBase
+      attr_reader :spi_port, :device_id
 
-    def initialize(pin_base, spi_port, device_id)
-      super
-      @pin_base = pin_base
-      @pin_count = 16
-      @spi_port = spi_port || 0
-      @device_id = device_id || 0
-      Wiringpi.mcp23s17Setup(pin_base, spi_port, device_id)
+      def initialize(pin_base, spi_port, device_id)
+        super
+        @pin_base = pin_base
+        @pin_count = 16
+        @spi_port = spi_port || 0
+        @device_id = device_id || 0
+        Wiringpi.mcp23s17Setup(pin_base, spi_port, device_id)
+      end
     end
   end
 end
