8401da15d2ceb3fae321e3f5d1341a42e9fd38dd
Rifqi Fauzi <rifqi.thefi.fauzi@gmail.com>
2021-07-01 12:58:48 +0700

    refreshing spi code

--- lib/wiringpi/spi.rb
+++ lib/wiringpi/spi.rb
@@ -1,15 +1,18 @@
 module WiringPi
   class SPI
-    @channel = 0
-	def initialize(channel,speed)
-	  @channel = channel
-	  Wiringpi.wiringPiSPISetup(channel,speed)
-	end
-	def wiringPiSPIGetFd()
-	  return Wiringpi.wiringPiSPIGetFd(@channel)
-	end
-	def wiringPiSPIDataRW(data)
-	  return Wiringpi.wiringPiSPIDataRW(@channel,data)
-	end
+		attr_reader :channel
+
+		def initialize(channel, speed)
+			@channel = channel
+			Wiringpi.wiringPiSPISetup(channel, speed)
+		end
+
+		def wiringPiSPIGetFd
+			Wiringpi.wiringPiSPIGetFd(@channel)
+		end
+
+		def wiringPiSPIDataRW(data)
+			Wiringpi.wiringPiSPIDataRW(@channel, data)
+		end
   end
 end
