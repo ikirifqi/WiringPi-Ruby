6191417f71746ddee1e6a317fb8d59cb27c93b44
Rifqi Fauzi <rifqi.thefi.fauzi@gmail.com>
2021-07-01 13:02:26 +0700

    refreshing serial code

--- lib/wiringpi/serial.rb
+++ lib/wiringpi/serial.rb
@@ -1,49 +1,39 @@
 module WiringPi
   class Serial
-
+    attr_reader :id, :device, :baud
     @id = 0
     @device = '/dev/ttyAMA0'
     @baud = 9600
 
-    def initialize(device='/dev/ttyAMA0', baud=9600)
-
+    def initialize(device = '/dev/ttyAMA0', baud = 9600)
+      @id = Wiringpi.serialOpen(device, baud)
       @device = device
       @baud = baud
-
-      @id = Wiringpi.serialOpen(@device, @baud)
-
     end
 
     def serial_close
-
       Wiringpi.serialClose(@id)
-      @id = 0
-
+      @id = nil
     end
 
     def serial_put_char(char)
-
+      raise 'serial connection is closed' if @id.nil?
       Wiringpi.serialPutchar(@id, char)
-
     end
 
     def serial_puts(string)
-
+      raise 'serial connection is closed' if @id.nil?
       Wiringpi.serialPuts(@id, string)
-
     end
 
     def serial_data_avail
-
+      raise 'serial connection is closed' if @id.nil?
       Wiringpi.serialDataAvail(@id)
-
     end
 
     def serial_get_char
-
+      raise 'serial connection is closed' if @id.nil?
       Wiringpi.serialGetchar(@id)
-
     end
-
   end
 end
